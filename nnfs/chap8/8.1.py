ca045e176e62e68f458569169bfefd8614acbdca
Patrik Olin <enmejl@gmail.com>
2022-02-04 16:40:20 +0100

    chapter 8 and 9.1

---
+++ nnfs/chap8/8.1.py
@@ -0,0 +1,55 @@
+
+# forward pass
+x = [1.0, -2.0, 3.0] # input values
+w = [-3.0, -1.0, 2.0] # weights
+b = 1.0 # bias
+
+# multiplying inputs by weights
+xw0 = x[0] * w[0]
+xw1 = x[1] * w[1]
+xw2 = x[2] * w[2]
+print(xw0, xw1, xw2)
+
+# adding weighted inputs and a bias
+z = xw0 + xw1 + xw2 + b
+print(z)
+
+# ReLU activation function
+y = max(z, 0)
+print(y)
+
+# backward pass
+
+# the derivative from the next layer
+dvalue = 1.0
+
+# derivative of the ReLU and the chain rule
+drelu_dz = dvalue * (1. if z > 0 else 0.)
+print(drelu_dz)
+
+# partial derivates of the multiplication, the chain rule
+dsum_dxw0 = 1
+dsum_dxw1 = 1
+dsum_dxw2 = 1
+dsum_db = 1
+drelu_dwx0 = drelu_dz * dsum_dxw0
+drelu_dwx1 = drelu_dz * dsum_dxw1
+drelu_dwx2 = drelu_dz * dsum_dxw2
+drelu_db = drelu_dz * dsum_db
+print(drelu_dwx0, drelu_dwx1, drelu_dwx2, drelu_db)
+
+
+# partial derivates of the multiplication, the chain rule
+dmul_dx0 = w[0]
+dmul_dx1 = w[1]
+dmul_dx2 = w[2]
+dmul_dw0 = x[0]
+dmul_dw1 = x[1]
+dmul_dw2 = x[2]
+drelu_dx0 = drelu_dwx0 * dmul_dx0
+drelu_dw0 = drelu_dwx0 * dmul_dw0
+drelu_dx1 = drelu_dwx1 * dmul_dx1
+drelu_dw1 = drelu_dwx1 * dmul_dw1
+drelu_dx2 = drelu_dwx2 * dmul_dx2
+drelu_dw2 = drelu_dwx2 * dmul_dw2
+print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
